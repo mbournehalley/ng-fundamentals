41f20c87975ec73e35b45b2d8985140a8896425b
Melbourne Lipardo <melbourne.lipardo@osi.ca.gov>
2019-05-09 12:01:20 -0700

    feat: added animations module and material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButton } from "@angular/material";
 import { NgModule } from "@angular/core";
 import { EventsAppComponent } from "./events-app.component";
 
 @NgModule({
   declarations: [EventsAppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatButton],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule {}
