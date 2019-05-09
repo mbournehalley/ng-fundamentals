02bf0772c67c2a624c65f1cef1b6f87d43381b88
Melbourne Lipardo <melbourne.lipardo@osi.ca.gov>
2019-05-09 11:48:11 -0700

    fix: change to events-app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { EventsAppComponent } from "./events-app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [EventsAppComponent],
+  imports: [BrowserModule],
+  bootstrap: [EventsAppComponent]
 })
-export class AppModule { }
+export class AppModule {}
